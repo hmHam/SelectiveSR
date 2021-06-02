270b785cd42bf3b32a5b8247d8225880885fa9f0
s-kato <s-kato@ar.sanken.osaka-u.ac.jp>
2021-06-03 00:59:22 +0900

    new metrics

--- src/get_agent_metrics.py
+++ src/get_agent_metrics.py
@@ -9,43 +9,46 @@ class Agent(object):
         self.actions = actions
         self.channel = channel
         
-    def __call__(self, y):
-        # 入力した画像をなおした結果と選んだ行動を返してほしい
+    def __call__(self, y, T=16):
         y = y.to(torch.float)
-        if self.channel == 1:
-            feature = y[None, None, ...]
-        elif self.channel == 2:
-            feature = torch.stack([y, y]).unsqueeze(0)
         took_actions = []
-        for _ in range(5):
+        # 入力が1チェネルの場合
+        if self.channel == 1:
+            for _ in range(T):
+                with torch.no_grad():
+                    q, _ = self.Qnet(y[None, None, ...])
+                    q = q[0]
+                    _, a = torch.max(q, axis=0)
+                    took_actions.append(a.item())
+                y = self.actions[a](y)
+            return y, took_actions
+        # 2チャネルの場合
+        s = torch.stack([y, y]).unsqueeze(0)
+        for _ in range(T):
             with torch.no_grad():
-                q, _ = self.Qnet(feature)
+                q, _ = self.Qnet(s)
                 q = q[0]
                 _, a = torch.max(q, axis=0)
                 took_actions.append(a.item())
-            if self.channel == 1:
-                feature = self.actions[a](feature.squeeze(0).squeeze(0))
-                feature = feature.unsqueeze(0).unsqueeze(0)
-            else:
-                y, z = feature.squeeze(0)
-                z = self.actions[a](z)
-                feature = torch.stack([y, z]).unsqueeze(0)
-        return feature if self.channel == 1 else feature[0][1], took_actions
+            z, y = s.squeeze(0)
+            z = self.actions[a](z)
+            s = torch.stack([z, y]).unsqueeze(0)
+        return s[0][0], took_actions
     
     
-def agent_metrics(test_dataset, origin, Qnet, action_candidates, channel=1, outdir=None, N=10000):
+def agent_metrics(Dy, Dx, Qnet, action_candidates, channel=1, outdir=None, N=10000):
     agent = Agent(Qnet, action_candidates, channel)
     
-    samples = []
+    metrics = []
     for i in tqdm(range(N)):
-        test_img = test_dataset[i]
-        o = origin[i]
+        test_img = Dy[i]
+        x = Dx[i]
         
-        agent_output, agent_took_actions = agent(test_img)
-        samples.append(
-            agent_took_actions + [torch.mean((agent_output-o)**2).item()]
+        agent_output, agent_took_actions = agent(test_img)  #復元
+        metrics.append(
+            agent_took_actions + [torch.mean((agent_output-x)**2).item()]
         )
-    metrics = np.array(samples)
+    metrics = np.array(metrics)
     if outdir is not None:
         np.save(metrics)
     return metrics
